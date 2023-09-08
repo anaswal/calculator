e884d93987f504cc91fcabea716afe5380ce7fd1
anaswal <anaswal321@gmail.com>
2023-09-08 18:51:22 +0800

    Adding AC and clear button

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const calculatorScreen = document.querySelector(".calculator-screen");
-const buttons = document.querySelectorAll("button");
+const inputScreen = document.querySelector(".input-screen");
+const currentScreen = document.querySelector(".current-screen");
+const numberButton = document.querySelectorAll(".number-button");
+const clearButton = document.querySelector(".clear-button");
+const acButton = document.querySelector(".ac-button");
+
+acButton.addEventListener("click", clearScreen);
+clearButton.addEventListener("click", deleteNumber);
+
+function deleteNumber() {
+  inputScreen.textContent = inputScreen.textContent.toString().slice(0, -1);
+}
+
+function clearScreen() {
+  inputScreen.textContent = "";
+  currentScreen.textContent = 0;
+}
 
 const appendNumber = (number) => {
-  calculatorScreen.textContent += number;
+  inputScreen.textContent += number;
 };
 
-buttons.forEach((button) => {
+numberButton.forEach((button) => {
   button.addEventListener("click", () => appendNumber(button.textContent));
 });
